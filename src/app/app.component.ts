765a3d7ba82afbd4a4a8d4f61071fe26abaa3dc0
anubis-cmyk <it19099132@my.sliit.lk>
2021-11-01 21:25:16 +0530

    create component dynamicaly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,24 @@ import { AdItem } from './ad-item';
 @Component({
   selector: 'app-root',
   template: `
+    
+    <button (click)="loadChildComponent();">Load</button>
+    <div *ngIf= 'loadComponent'>
     <div>
       <app-ad-banner [ads]="ads"></app-ad-banner>
+    </div>
     </div>
   `
 })
 export class AppComponent implements OnInit {
   ads: AdItem[] = [];
+  loadComponent = false;
 
   constructor(private adService: AdService) {}
 
+  loadChildComponent(){ 
+       this.loadComponent = true; 
+    }
   ngOnInit() {
     this.ads = this.adService.getAds();
   }
